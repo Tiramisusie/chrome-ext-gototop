678869eba4852ce22af3e33c07c26b107e242710
梁宁聪 <Tiramisusie@icloud.com>
2015-10-05 16:15:16 +0800

    添加了滚动效果

--- js/top.js
+++ js/top.js
@@ -1,11 +1,24 @@
-var body = document.body;
-body.setAttribute( 'id', 'congsusie' );
+//滚动动画
+var scrolling = function(e){
+    e.preventDefault();
+    var timer;
+
+    timer = setInterval(function () {
+        if( document.body.scrollTop > 0 ){
+            document.body.scrollTop -= 100;
+        } else {
+            document.body.scrollTop = 0;
+            clearInterval( timer );
+        }
+    }, 30 );
+};
 
 var img = document.createElement('img');
 img.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAbwAAAG8B8aLcQwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAGWSURBVFiF7Ze/S8NAFMc/jS0q9A/wLzABVzWLlooiiHVy8B9w0s2/wsmtmzgquIhQnERQB73BQaFwp5sOooIiiGiD1CERquR6TUybDv1Ahty9e++bdz9eLmMPj+eBVaAE5OgMHlABytkg+HqHAjcyAWDhf3lalCw6l/YwclaKwQF/ClIl26RvGagmFGcE2IwqoCqVOE8iumO72r6unoI9x3Y/DeO9FvwA9Gva680GDhmc1oAl4AvYBQYM9mFkTMp1fACLUokDAMd254F9IB/VUZw18A4s/AQHkEocATPAS7sFvAFzUonDvx1SCQEUgYd2CXgFZqUSJzoDqcQVMAncJi3gGZiWSpyZDKUSN/iV7jopAU/AlFTiohWHgYg7/Exc/lfAPVAMUhsJqcQj/ppomjXTNtwBCo7tFhratqQStTBjx3b7gRWgr+E5BUbRlH2TgLWQtm38QyiMQWDD4PMXcc6BeowxPQE9AVriVMMxx3Z1uyByNYwj4DjGGC2p/5J1hQDPaNU+PAv/lpoWlSxQDl5SuZ5/A+vkZ0FXwYF3AAAAAElFTkSuQmCC';
 
 var a = document.createElement('a');
-a.setAttribute( 'href', '#congsusie' );
+a.setAttribute( 'href', '#' );
+a.addEventListener( 'click', scrolling );
 a.style.position = 'fixed';
 a.style.right = '100px';
 a.style.bottom = '100px';
@@ -14,7 +27,7 @@ a.style.zIndex = '999999';
 
 a.appendChild(img);
 
-body.appendChild(a);
+document.body.appendChild(a);
 
 window.onscroll = function(){
     if(window.pageYOffset >= 200){
